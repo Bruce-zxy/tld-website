a1a0616b00a50af284c4966925fdc7d0a61da064
Bruce-zxy <bruce_zxy@163.com>
2018-12-11 10:24:06 +0800

    delete jump link for mobile page

--- js/index.js
+++ js/index.js
@@ -45,9 +45,4 @@ $(function() {
             }, false);
         }
     })(750, 750);
-
-    if ((!navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-        document.write("请访问PC端以获取更好的用户体验！");
-    }
-
 });
